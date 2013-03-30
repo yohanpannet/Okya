3e237ea97a0392fade26ceb81f3f9ed80cf47095
unknown <Yohan@.(none)>
2013-03-30 15:13:05 +0100

    finish the table thing

--- js/game.js
+++ js/game.js
@@ -3,30 +3,39 @@
  */
 
 
+
 function buildBoard() {
 	//create tile index list for random pick up
-	tileIndexes=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-	
-	var elt = document.getElementById("board");
-	elt.innerHTML= '';
-	for (var i=0; i<16;i++){
-		//get random tile
-		randIndex = Math.floor(Math.random() * tileIndexes.length);
-		var randIndex = tileIndexes.splice(randIndex,1)[0];
-		var tile = tiles[randIndex];
-		
-		var div = document.createElement('div');
-		div.setAttribute('class', 'tile');
-		div.setAttribute('droppable', true);
-		div.setAttribute('taken', false); 
-		div.setAttribute('fauna', tile.x);
-		div.setAttribute('flora', tile.y);
-		div.innerHTML='<header>'+tile.x+' </header><header>'+tile.y+'</header>';
-		tile.elt=div;
-		elt.appendChild(div);
-		
+	tileIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+	var board = document.getElementById("board");
+	board.innerHTML = '';
+	for (var i = 0; i < 4; i++) {
+		var line = document.createElement('tr');
+		for (var j = 0; j < 4; j++) {
+			var col = document.createElement('td');
+			//get random tile
+			randIndex = Math.floor(Math.random() * tileIndexes.length);
+			var randIndex = tileIndexes.splice(randIndex,1)[0];
+			var tile = tiles[randIndex];
+
+			var div = document.createElement('div');
+			div.setAttribute('class', 'tile');
+			div.setAttribute('droppable', true);
+			div.classList.add('droppable');
+			div.setAttribute('taken', false);
+			div.setAttribute('fauna', tile.x);
+			div.setAttribute('flora', tile.y);
+			div.innerHTML = '<header>' + tile.x + ' </header><header>' + tile.y + '</header>';
+			tile.elt = div;
+			col.appendChild(div);
+			line.appendChild(col);
+		}
+		board.appendChild(line);
 	}
+
 }
+
 
 function createTiles(){
 	for (var i = 0; i < 4; i++) { 

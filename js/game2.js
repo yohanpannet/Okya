502a6e54e0f4ab42f8ff92bb90a85dcff1622488
yohan pannet <yohan.pannet@gmail.com>
2014-05-19 11:43:22 +0200

    moving to mobile

--- js/game2.js
+++ js/game2.js
@@ -1,6 +1,8 @@
 var party='';
 
 function startNewGame(){
+	console.log('startGame');
+	$.mobile.changePage('#pagegame');
 	gameController.newGame();
 	
 }
--- js/view.js
+++ js/view.js
@@ -9,7 +9,7 @@ function BoardView(){
 				var tile = modelBoard[i][j];
 				var col = $('<td>');
 				var div = $('<div>',{
-					'class': 'tile ',
+					'class': 'tile',
 						//+'prop1'+tile.prop1+' prop2'+tile.prop2,
 					'id':'tile'+i+j,
 					'taken': false,
